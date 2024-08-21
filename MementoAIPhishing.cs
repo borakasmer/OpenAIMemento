7b7175bfa105bd3a4540a95377fc1b8d9d1e99da
Bora KAŞMER <bora@borakasmer.com>
2024-08-21 21:04:48 +0300

    Create Project

---
+++ MementoAIPhishing.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MementoAI
+{
+    public class MementoAIPhishing
+    {
+        public string MailTemplate { get; set; }
+        MailRedoTaker RT;
+        MailUndoTaker UT;
+        public MementoAIPhishing()
+        {
+            RT = new();
+            UT = new();
+        }
+        public void Save()
+        {
+            UT.Mail = new AIMailMemento() { MailTemplate = MailTemplate };
+        }
+        public void Undo()
+        {
+            RT.Mail = new AIMailMemento() { MailTemplate = this.MailTemplate };
+            this.MailTemplate = UT.Mail.MailTemplate;
+        }
+        public void Redo()
+        {
+            UT.Mail = new AIMailMemento() { MailTemplate = this.MailTemplate };
+            this.MailTemplate = RT.Mail.MailTemplate;
+        }
+
+        public class MailUndoTaker
+        {
+            Stack<AIMailMemento> mementos = new Stack<AIMailMemento>();
+            public AIMailMemento Mail
+            {
+                get => mementos.TryPop(out AIMailMemento result) ? result : new AIMailMemento();
+                set
+                {
+                    mementos.Push(value);
+                }
+            }
+        }
+        public class MailRedoTaker
+        {
+            Stack<AIMailMemento> mementos = new Stack<AIMailMemento>();
+            public AIMailMemento Mail
+            {
+                get => mementos.TryPop(out AIMailMemento result) ? result : new AIMailMemento();
+                set
+                {
+                    mementos.Push(value);
+                }
+            }
+        }
+        public class AIMailMemento
+        {
+            public string MailTemplate { get; set; }
+            public DateTime CreatedDate { get; set; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using OpenAI.Managers;
+using OpenAI.ObjectModels.RequestModels;
+using OpenAI.ObjectModels;
+using OpenAI;
+using MementoAI;
+using OpenAI.ObjectModels.ResponseModels;
+
+string tags = "Contains Credit and Car Tags.";
+string prompt = "Write an email to Bora Kasmer offering the recipient a chance to win a $50\\r\\nAmazon gift card when they click a link to complete an employee satisfaction survey. Let the connection path be https://borakasmer.com?id=5. Create a beautifully formatted output using HTML and CSS.\", string whatyouare = \"You are a helpful assistant";
+string whatyouare = "You are a helpful assistant.";
+
+MementoAIPhishing memento = new();
+//memento.MailTemplate = "1";
+//memento.Save();
+//memento.MailTemplate = "2";
+//memento.Save();
+//memento.MailTemplate = "3";
+for (var i = 0; i < 3; i++)
+{
+    var result = GetPhishingMail(tags, prompt, whatyouare);
+    memento.MailTemplate = result;
+    if (i != 2) memento.Save();
+}
+Console.WriteLine(memento.MailTemplate);
+Console.WriteLine("");
+Console.WriteLine("Undo".PadLeft(40, '*'));
+memento.Undo();
+Console.WriteLine(memento.MailTemplate);
+Console.WriteLine("Undo".PadLeft(40, '*'));
+memento.Undo();
+Console.WriteLine(memento.MailTemplate);
+Console.WriteLine("Redo, Redo".PadLeft(40, '*'));
+memento.Redo();
+memento.Redo();
+Console.WriteLine(memento.MailTemplate);
+
+//Test For Null State
+memento.Redo();
+Console.WriteLine(String.IsNullOrEmpty(memento.MailTemplate)?"No Result": memento.MailTemplate);
+
+Console.ReadLine();
+string GetPhishingMail(string tags, string prompt, string whatyouare)
+{
+    var openAiService = new OpenAIService(new OpenAiOptions()
+    {
+        ApiKey = "---YOUR PASSWORD---"
+    });
+
+    var completionResult = openAiService.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest()
+    {
+        Messages = new List<ChatMessage>
+        {
+            ChatMessage.FromSystem(whatyouare),
+            ChatMessage.FromUser(prompt),
+            ChatMessage.FromUser(tags),
+
+        },
+
+        Model = Models.Gpt_4_turbo,
+        MaxTokens = 2000,
+        FrequencyPenalty = 0,
+        Temperature = (float?)0.7,
+        PresencePenalty = 0,
+    });
+
+    if (completionResult.Result.Successful)
+    {
+        return completionResult.Result.Choices.FirstOrDefault().Message.Content;
+    }
+    else
+    {
+        if (completionResult.Result.Error == null)
+        {
+            throw new Exception("Unknown Error");
+        }
+        return $"{completionResult.Result.Error.Code}: {completionResult.Result.Error.Message}";
+    }
+}
